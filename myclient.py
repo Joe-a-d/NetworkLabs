1833be3645a435e427606dcb26a3af678659ac7f
Joe-a-d <2334590D@student.gla.ac.uk>
2019-10-16 15:15:12 +0100

    add input capability, exit check; Broken Pipe after second message

--- myclient.py
+++ myclient.py
@@ -10,5 +10,11 @@ import sys
 ##############
 cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cli_sock.connect((sys.argv[1], int(sys.argv[2])))
-cli_sock.sendall(sys.argv[3].encode['utf-8'])
-cli_sock.close()
+
+connected = True
+while connected:
+    clientMessage = str(input())
+    cli_sock.sendall(clientMessage.encode("utf-8"))
+    if clientMessage == "EXIT":
+        connected = False
+        cli_sock.close()
