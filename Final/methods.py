1b5a4052f9ee10aaf78f099efd4dfc9ea02e2a98
Joe-a-d <2334590D@student.gla.ac.uk>
2019-10-30 20:21:37 +0000

    add receive_file method without tests

--- Final/methods.py
+++ Final/methods.py
@@ -7,9 +7,10 @@ def send_file(file,socket):
 		with open(file, 'b') as f:
 		    data = f.read()
 	except:
-		print(file + " does not exist or is not your current working directory")
+		print(file + " is not your current working directory")
 
 	try:
+		socket.sendall((str(size)+":").encode('')) # first, send packet with size of file to check against corruption
 		socket.sendall(data)
 	except Exception as e:
 		print(e);
@@ -17,8 +18,20 @@ def send_file(file,socket):
 	return 0
 
 
-def rec_file():
-	return
+def rec_file(file,socket):
+	while ":" not in socket.recv():
+		size = socket.recv()
+	recd = 0
+	chunks = []
+	while recd < size:
+        chunk = self.sock.recv(4096)
+        if chunk == b'':
+            raise RuntimeError("socket connection broken")
+        recd += len(chunk)
+	with open(file , 'xb') as f:
+		f.write(b''.join(chunks))
+
+	return 0
 
 
 def send_dirs():
@@ -26,3 +39,8 @@ def send_dirs():
 
 def rec_dirs():
 	return
+
+### bibliography
+ # https://docs.python.org/3.3/howto/sockets.html
+ # https://stackoverflow.com/questions/25435212/is-there-a-way-to-check-whether-the-data-buffer-for-a-socket-is-empty-or-not-in
+ # https://stackoverflow.com/questions/41382127/how-does-the-python-socket-recv-method-know-that-the-end-of-the-message-has-be
--- Final/server.py
+++ Final/server.py
@@ -5,61 +5,33 @@ from lab3 import socket_to_screen, keyboard_to_socket
 # Create the socket on which the server will receive new connections
 srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-""" 
- Enclose the following two lines in a try-except block to catch
- exceptions related to already bound ports, invalid/missing
- command-line arguments, port number out of range, etc. Ideally,
- these errors should have been handled separately.
-"""
+
 try:
-	"""
-	 Register the socket with the OS kernel so that messages sent
-	 to the user-defined port number are delivered to this program.
-	 Using "0.0.0.0" as the IP address so as to bind to all available
-	 network interfaces. Alternatively, could have used "127.0.0.1"
-	 to bind only to the local (loopback) interface, or any other IP
-	 address on an interface of the computer where this program is
-	 running (use "ipconfig /all" to list all interfaces and their IP
-	 addresses).
-	"""
-	srv_sock.bind(("0.0.0.0", int(sys.argv[1]))) # sys.argv[1] is the 1st argument on the command line
-	
-	"""
-	 Create a queue where new connection requests will be added by
-	 the OS kernel. This number should be small enough to not waste
-	 resources at the OS level, but also large enough so that the
-	 connections queue doesn't fill up. For this latter, one should
-	 ideally have an idea of how long it takes to serve a request
-	 and how frequently clients initiate new connections to the
-	 server.
-	"""
+	# creates a socket with port == argument passed on CLI
+	srv_sock.bind(("0.0.0.0", int(sys.argv[1])))
+
+
+    # accepts a max of 5 connections
 	srv_sock.listen(5)
 except Exception as e:
-	# Print the exception message
+
 	print(e)
 	# Exit with a non-zero value, to indicate an error condition
 	exit(1)
 
-# Loop forever (or at least for as long as no fatal errors occur)
-while True:
-	"""
-	 Surround the following code in a try-except block to account for
-	 socket errors as well as errors related to user input. Ideally
-	 these error conditions should be handled separately.
-	"""
-	try:
-		print("Waiting for new client... ")
-		
-		"""
-		 Dequeue a connection request from the queue created by listen() earlier.
-		 If no such request is in the queue yet, this will block until one comes
-		 in. Returns a new socket to use to communicate with the connected client
-		 plus the client-side socket's address (IP and port number).
-		"""
-		cli_sock, cli_addr = srv_sock.accept()
-		cli_addr_str = str(cli_addr) # Translate the client address to a string (to be used shortly)
 
-		print("Client " + cli_addr_str + " connected. Now chatting...")
+while True:
+
+	try:
+        print("Connection for " + socket.gethostbyname("0.0.0.0") + " established on port  " + sys.argv[1]) # acknowledges connection
+		print("Waiting for new client... ")
+
+
+		cli_sock, cli_addr = srv_sock.accept()
+		cli_addr_str = str(cli_addr) # Translate the client address to a string
+
+
+		print("Client " + cli_addr_str + " connected.")
 
 		# Loop until either the client closes the connection or the user requests termination
 		while True:
@@ -76,10 +48,7 @@ while True:
 				break
 
 	finally:
-		"""
-		 If an error occurs or the client closes the connection, call close() on the
-		 connected socket to release the resources allocated to it by the OS.
-		"""
+
 		cli_sock.close()
 
 # Close the server socket as well to release its resources back to the OS
