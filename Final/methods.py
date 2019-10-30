7b584d4ba67d1ed52159f6b2c081e9a5d1d570b9
Joe-a-d <2334590D@student.gla.ac.uk>
2019-10-30 21:26:36 +0000

    amend send_file ; passing unit tests

--- Final/methods.py
+++ Final/methods.py
@@ -1,44 +1,50 @@
 import sys
+import os
+from os import path
 import socket
+### add print time for each print
 
 def send_file(file,socket):
-	try:
-		size = os.path.getsize(file)
-		with open(file, 'b') as f:
-		    data = f.read()
-	except:
-		print(file + " is not your current working directory")
+    try:
+        size = path.getsize(file)
+        with open(file, 'rb') as f:
+            data = f.read()
+    except Exception as e:
+        print(e)
+        print(file + " is not in your current working directory")
 
-	try:
-		socket.sendall((str(size)+":").encode('')) # first, send packet with size of file to check against corruption
-		socket.sendall(data)
-	except Exception as e:
-		print(e);
+    try:
+        print('in sned')
+        socket.sendall(str.encode(str(size)+":")) # first, send packet with size of file to check against corruption
+        socket.sendall(data)
+        print(data)
+        return 0
+    except Exception as e:
+        print(e);
 
-	return 0
 
 
 def rec_file(file,socket):
-	while ":" not in socket.recv():
-		size = socket.recv()
-	recd = 0
-	chunks = []
-	while recd < size:
+    while ":" not in socket.recv():
+        size = socket.recv()
+    recd = 0
+    chunks = []
+    while recd < size:
         chunk = self.sock.recv(4096)
         if chunk == b'':
             raise RuntimeError("socket connection broken")
         recd += len(chunk)
-	with open(file , 'xb') as f:
-		f.write(b''.join(chunks))
+    with open(file , 'xb') as f:
+        f.write(b''.join(chunks))
 
-	return 0
+    return 0
 
 
 def send_dirs():
-	return
+    return
 
 def rec_dirs():
-	return
+    return
 
 ### bibliography
  # https://docs.python.org/3.3/howto/sockets.html
